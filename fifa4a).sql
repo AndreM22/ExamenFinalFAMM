1ae269825191ad3328006e9d626636b646376f1a
element222 <fabian.machicao@gmail.com>
2020-03-24 18:49:11 -0400

    Pregunta 4 inciso a)

--- fifa4a).sql
+++ fifa4a).sql
@@ -1,3 +1,3 @@
 -- a) Contar el numero de argentinos que juegan en FC Barcelona
 select count(personal_details.id) from personal_details
-inner join other_details on personal_details.player_id = other_details.player_id and other_details.club = 'FC Barcelona'
+inner join other_details on personal_details.player_id = other_details.player_id and other_details.club = 'FC Barcelona' and personal_details.nationality = 'Argentina'
